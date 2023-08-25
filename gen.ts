5b172be70d6daf84f33dc9be953fb8c959c3129b
Gnlow <gnlow@protonmail.com>
2023-08-26 04:32:39 +0900

    complete

--- gen.ts
+++ gen.ts
@@ -11,52 +11,56 @@ const unit =
     (
         n: number,
         x: number,
-        y: number,
         shape: Shape
     ) =>
     ""
     +`{{#ifeq: {{#expr: floor( {{{1|1}}} / 2^${n} ) mod 2 }} | 1 <!--\n-->`
-    +`    | <div style="margin: ${80*y-100}px 0 0 ${80*x}px;">`
+    +`    | <div style="margin: -100px 0 0 ${80*x}px;">`
         +`[[그림:menslengeus-${shapeToNum(shape)}.svg|alt=${shape.replace("|", "{{!}}")}]]`
         +`</div> <!--\n-->`
     +`}}`
 
 const units =
-    (as: [x: number, y: number, shape: Shape][]) =>
-    as  .map(([x, y, shape], n, l) =>
-            unit(n, x, y-(l[n-1]?.[1] || 0), shape)
+    (start: number, as: [x: number, shape: Shape][]) =>
+    as  .map(([x, shape], n) =>
+            unit(start+n, x, shape)
         )
         .join("<!--\n-->")
 
 const result =
 `<onlyinclude>
 <div style="width: 100px; height: 100px;"> </div><!--
--->${units([
-    [0, 0, "-"],
-    [1, 0, "-"],
-    [0, 0, "|"],
-    [0, 0, "\\"],
-    [0, 0, "/"],
-    [1, 0, "|"],
-    [1, 0, "\\"],
-    [1, 0, "/"],
-    [2, 0, "|"],
-    [0, 1, "-"],
-    [1, 1, "-"],
-    [0, 1, "|"],
-    [0, 1, "\\"],
-    [0, 1, "/"],
-    [1, 1, "|"],
-    [1, 1, "\\"],
-    [1, 1, "/"],
-    [2, 1, "|"],
-    [0, 2, "-"],
-    [1, 2, "-"],
+-->${units(0,[
+    [0, "-"],
+    [1, "-"],
+    [0, "|"],
+    [0, "\\"],
+    [0, "/"],
+    [1, "|"],
+    [1, "\\"],
+    [1, "/"],
+    [2, "|"],
+])}<div style="width: 100px; height: 100px; margin: -20px 0 0 0;"> </div><!--
+-->${units(9,[
+    [0, "-"],
+    [1, "-"],
+    [0, "|"],
+    [0, "\\"],
+    [0, "/"],
+    [1, "|"],
+    [1, "\\"],
+    [1, "/"],
+    [2, "|"],
+])}<div style="width: 100px; height: 100px; margin: -20px 0 0 0;"> </div><!--
+-->${units(18,[
+    [0, "-"],
+    [1, "-"],
 ])}
 </onlyinclude>
 
 ---
-{{코드 보여주기||<nowiki>{{:연습장:Gnlow/Expr|635371}}</nowiki>}}
+{{코드 보여주기||<nowiki>{{:연습장:Gnlow/Expr|364896}}</nowiki>}}
+{{코드 보여주기||<nowiki>{{:연습장:Gnlow/Expr|1048575}}</nowiki>}}
 `
 
 await Deno.writeTextFile("main.mw", result)
